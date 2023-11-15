7151882fb243d973444efd9768d4a5297559459c
jatnika <jatnika026@gmail.com>
2023-11-15 10:12:15 +0700

    update

--- contoh.sh
+++ contoh.sh
@@ -1,30 +1,32 @@
 #!/bin/bash
 
 # Initialize variables with default values
-PARAM1=""
-PARAM2=""
+DIR=""
+NAME=""
 
 # Usage function
 usage() {
-    echo "Usage: custom_command.sh -p <param1> -q <param2>"
+    echo "Usage: custom_command.sh -dir <DIR> -name <NAME>"
     exit 1
 }
 
 # Parse options using getopts
-while getopts ":p:q:" opt; do
+while getopts ":dir:name:" opt; do
     case $opt in
-        p) PARAM1="$OPTARG";;
-        q) PARAM2="$OPTARG";;
+        dir) DIR="$OPTARG";;
+        name) NAME="$OPTARG";;
         \?) echo "Invalid option -$OPTARG" >&2; usage;;
     esac
 done
 
 # Check if required parameters are provided
-# if [ -z "$PARAM1" ] || [ -z "$PARAM2" ]; then
+# if [ -z "$DIR" ] || [ -z "$NAME" ]; then
 #     echo "Both parameters are required."
 #     usage
 # fi
 
+git clone https://github.com/enjat26/generate-module-flutter.git
+mv generate-module-flutter/name_controller.dart generate-module-flutter/"$NAME"_controller.dart 
 # Display the received parameters
-echo "Parameter 1: $PARAM1"
-echo "Parameter 2: $PARAM2"
+echo "Parameter 1: $DIR"
+echo "Parameter 2: $NAME"
